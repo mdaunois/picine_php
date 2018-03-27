f5f8cd619ae4eb22190d2dc938b0c64fbe384e8b
mdaunois <mathieudaunois@gmail.com>
2018-03-27 14:15:39 +0200

    fin d02

--- d02/ex03/who.php
+++ d02/ex03/who.php
@@ -4,25 +4,30 @@ date_default_timezone_set('Europe/paris');
 $usr = get_current_user();
 $file = file_get_contents("/var/run/utmpx");
 $sub = substr($file, 1256);
-$e = array();
-$typedef   = 'a256user/a4id/a32line/ipid/itype/I2time';
+$tab = array();
+$format = 'a256user/a4id/a32line/ipid/itype/I2time';
 while ($sub != NULL) {
-	$array = unpack($typedef, $sub);
-	print_r($array);
-	if (strcmp(trim($array[user]), $usr) == 0 && $array[type] == 7)
+	$info = unpack($format, $sub);
+	if (strcmp(trim($info[user]), $usr) == 0 && $info[type] == 7)
 	{
-		$date = date("M  j H:i", $array["time1"]);
-		$line = trim($array[line]);
-		$line = $line . "  ";
-		$usrr = trim($array[user]);
-		$usrr = $usrr . "  ";
-		$tab = array($usrr.$line.$date);
-		$e = array_merge($e, $tab);
+		$date = date("M j H:i", $info["time1"]);
+		if ($date[5] == ' ')
+		{
+			$deb = substr($date, 0, 3)."  ".substr($date,4);
+			$date = $deb;
+		}
+		$line = trim($info[line]);
+		$line = $line ."  ";
+		$usrr = trim($info[user]);
+		$usrr = $usrr ." ";
+		$str = array($usrr.$line.$date);
+		$tab = array_merge($tab, $str);
 	}
 	$sub = substr($sub, 628);		
 }
-sort($e);
-foreach ($e as $elem) {
+sort($tab);
+foreach ($tab as $elem)
+{
 	echo $elem;
 	echo "\n";
 }
